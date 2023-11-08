e3c06f730873606ac788c84fe91884a3322ccc43
Trovoada01 <112417747+Trovoada01@users.noreply.github.com>
2023-11-08 07:47:50 -0300

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/estilos.css">
+    <style>
+        h3{
+    color:brown;
+    border-bottom:1px solid;
+    }
+.container{
+    margin:auto;
+    width: 1200px; 
+}
+body{
+    background-color: aquamarine;
+
+}
+    </style>
+    <title>Home page</title>
+</head>
+<body>
+    <section class="container">
+    <h3>Lucas Daniel</h3>
+    <img src="/imagens/sim.png" alt="">
+    </section>
+</body>
+</html>
